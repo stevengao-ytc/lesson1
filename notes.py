66585b46ab1ab64eb84eebe18d5714155bf31063
stevengao-ytc <stevengao213@gmail.com>
2026-05-02 12:33:50 +1100

    Create notes.py

---
+++ notes.py
@@ -0,0 +1,19 @@
+# this is a brief summary of the lesson
+# we have learned: strings, integers, floats, booleans, str() method, user input
+
+# examples:
+food = "pizza"  # string
+text = "BOTW is an amazing video game!"  # string
+number = 5  # integer
+decimal = 5.5  # float
+is_sunny = True  # boolean
+
+print(type(food))  # to check a type of a variable
+
+# answer for the little exercise in class:
+name = input("What is your name?")
+age = input("What is your age?")
+print("I am " + name + ". " + "I am " + age + " years old.")
+
+
+
